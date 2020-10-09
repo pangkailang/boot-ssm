b680ad244c9d828916b2ca8979507c5b3da275ed
pangkailang <330891873@qq.com>
2020-10-09 19:41:46 +0800

    初始化工程

---
+++ src/main/java/com/pkl/boot/BootSsmApplication.java
@@ -0,0 +1,13 @@
+package com.pkl.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pkl/boot/BootSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkl.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
